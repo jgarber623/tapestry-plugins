6a5a7125b35ada4ce4b97aecff089b5585b2c99b
Jason Garber <jason@sixtwothree.org>
2024-02-12 23:56:26 -0500

    🧶 Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Project Tapestry plugins
+
+Custom plugins for The Iconfactory's [Project Tapestry](https://www.kickstarter.com/projects/iconfactory/project-tapestry) application.
+
+Official documentation for Project Tapestry's plugin system may be found in the [TheIconfactory/Tapestry](https://github.com/TheIconfactory/Tapestry) repository.
+
+| Name           | Identifier           | Description                                    |
+|:---------------|:---------------------|:-----------------------------------------------|
+| Bandcamp Daily | `com.bandcamp.daily` | Posts published by Bandcamp's editorial staff. |
---
+++ com.bandcamp.daily/plugin.js
@@ -0,0 +1,39 @@
+function identify() {
+  setIdentifier(null);
+}
+
+function load() {
+  loadAsync()
+    .then(processResults)
+    .catch(processError);
+}
+
+async function loadAsync() {
+  const text = await sendRequest("https://daily.bandcamp.com/feed");
+  const obj = xmlParse(text);
+
+  const link = obj.rss.channel.link;
+
+  return obj.rss.channel.item.map(item => {
+    const creator = Creator.createWithUriName(link, item["dc:creator"]);
+
+    creator.avatar = "https://s4.bcbits.com/img/favicon/apple-touch-icon.png";
+
+    const matches = item.description.match(/<p>.*?<\/p>/g);
+    const [_, imageUrl] = matches[0].match(/<img\s+(?:.+)?src="(.+?)"(?:\s+.+)?>/);
+
+    const content = (imageUrl ? matches.slice(1) : matches).join('');
+
+    const post = Post.createWithUriDateContent(item.link, new Date(item["dc:date"]), content);
+
+    if (imageUrl) {
+      const attachment = Attachment.createWithMedia(imageUrl);
+
+      post.attachments = [attachment];
+    }
+
+    post.creator = creator;
+
+    return post;
+  });
+}
